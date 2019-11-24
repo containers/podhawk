0708e70b77ff5e02494f6d3291fde8b274ac64a4
Florent H. CARRÉ <colundrum@users.noreply.github.com>
2019-11-24 00:23:54 -0500

    first part done : get all values to update images

--- code.py
+++ code.py
@@ -1,69 +1,63 @@
-import subprocess
-import os
-images = subprocess.Popen(["podman","images"],   
-            stdout=subprocess.PIPE, 
-            stderr=subprocess.STDOUT)
-stdout,stderr = images.communicate()
-stdout=stdout.split()
-imagelist=[]  #This stores the list of images currently in the local machine
-for i in range(6, len(stdout), 8):
-	imagelist.append(stdout[i])
+from json import loads
+from subprocess import run
+from typing import List
 
-containers = subprocess.Popen(["podman","ps"],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT)
-stdout,stderr = containers.communicate()
-
-containerlist=[]  #This list stores the names of all the running containers
-runningimages=[]  #This list stores the names of all the running images
-count=1
-for item in stdout.split('\n'): #Spliting on the basis of new line
-	if count==1:		#Skips the first line which contains the column names
-		count=count+1
-		continue
-	else:
-		if len(item)<=1: 
-			continue
-		listing=item.split(' ') #Splits on the basis of space
-		#x=listing[2].split(':') #This line can be uncommented if you want to download the image with the latest tag irrespective of the current tag
-		
-		containerlist.append(listing[0])
-		runningimages.append(listing[2])
-		#runningimages.append(x[0]) #This line can be uncommented and the previous line has to be commented to download the image with the latest tag irrespective of the current tag
+ctn_list: List[tuple] = []
+img_id_name_tag: List[tuple] = []
+img_updated: List[str] = []
 
 
-containerinfo=[]  #Stores the image ids of the images which are currently running on the containers
+def update_img(data):
+    """update image using podman pull and if updated append it to
+    global var to recreate containers
 
-for i in range(0, len(containerlist)):
-	temp_info = subprocess.Popen(["podman","inspect",containerlist[i]],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT)
-	stdout,stderr = temp_info.communicate()
-	for item in stdout.split('\n'):
-		if "Image" in item:
-			x=item.strip()
-			break
-	x=x.split(':')
-	x=x[1].split('"')
-	x=x[1]
-	containerinfo.append(x)
+    Args:
+        data (list): image id, name and tag
+    """
+    img: tuple
+    for img in data:
+        img_name: str = img[1]
+        old_id: str = img[0]
+        pull_output: str = run(
+            ['podman', 'pull', '-q', img_name],
+            capture_output=True).stdout.decode('utf-8').rstrip()
+        if pull_output != old_id:
+            img_updated.append(img_name)
 
-newimages=[]		#This list stores all the new pulled images corresponding to the running images
-for i in range(0, len(runningimages)):
-	pull = subprocess.Popen(["podman","pull",runningimages[i]],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT)
-        stdout,stderr = pull.communicate()
-	stdout=stdout.split('\n')
-	stdout=stdout[-2]
-	newimages.append(stdout)	
 
-for i in range(0, len(containerlist)):
-	if(containerinfo[i]!=newimages[i]): 				#Compares the new image ids with the current image ids
-		os.system("podman "+"stop "+containerlist[i])		#Stopping the container
-		os.system("podman "+"rm "+containerlist[i])		#Deleting the container
-		pull = subprocess.Popen(["podman","create",runningimages[i]],	#Create a new container with the new image
-            		stdout=subprocess.PIPE,
-            		stderr=subprocess.STDOUT)
-        	stdout,stderr = pull.communicate()
-		os.system("podman "+"start "+stdout)				#Starting the new container
+def identify_img_name_tag(data):
+    """extract image name with tag from podman images (json) and
+    append it to global var for later use
+
+    Args:
+        data (list): json from podman images
+    """
+    for image in data:
+        if image['names'] is not None:
+            img_id_name_tag.append((str(image['id']), image['names'][0]))
+
+
+def prepare_containers_list(data):
+    """prepare global var used to recreate running container when
+    image's updated
+
+    Args:
+        data (list): json from podman ps
+    """
+    for container in data:
+        if 'Up' in container['Status']:
+            ctn_list.append((str(container['ID']), container['Image']))
+
+
+# get current containers
+ps_output = run(['podman', 'ps', '--format', 'json'],
+                capture_output=True).stdout.decode('utf-8')
+prepare_containers_list(loads(ps_output))
+
+# get current images
+images_output = run(['podman', 'images', '--format', 'json'],
+                    capture_output=True).stdout.decode('utf-8')
+identify_img_name_tag(loads(images_output))
+
+# pull images
+update_img(img_id_name_tag)
